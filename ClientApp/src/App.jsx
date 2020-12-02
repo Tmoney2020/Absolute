6ec3e8aebeb997528422f03c4007a9e832644445
Trent Costa <trentcosta@gmail.com>
2020-12-02 15:39:32 -0500

    Created Nav bar

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,24 +1,44 @@
-import React, { Component } from 'react'
-import { Route, Switch } from 'react-router'
-import { Layout } from './components/Layout'
-import { Home } from './pages/Home'
-import HelloWorld from './pages/_template/HelloWorld'
-import HeyWorld from './pages/_template/HeyWorld'
-import NotFound from './pages/NotFound'
 import './custom.scss'
-export default class App extends Component {
-  static displayName = App.name
+import React, { useState } from 'react'
 
-  render() {
-    return (
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/counter" component={HelloWorld} />
-          <Route exact path="/typescript" component={HeyWorld} />
-          <Route exact path="*" component={NotFound} />
-        </Switch>
-      </Layout>
-    )
-  }
+
+export function App() {
+  const [navbarIsOpen, setNavbarIsOpen] = useState(false)
+
+
+  return (
+<nav className="navbar navbar-expand-lg navbar-light bg-primary">
+    <div className="d-flex flex-grow-1">
+        <span className="w-100 d-sm-none d-block text-left"/>
+        <a className="navbar-brand d-lg-inline-block text-light" href="#">
+            Absolute Kitchen & Flooring
+        </a>
+        <div className="w-100 text-right">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar" onClick={() => setNavbarIsOpen(!navbarIsOpen)}>
+                <span className="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </div>
+    <div className={`collapse navbar-collapse ${
+                navbarIsOpen ? 'show' : ''
+              }`}flex-grow-1 text-right id="myNavbar">
+
+
+        <ul className="navbar-nav ml-auto flex-nowrap">
+            <li className="nav-item">
+                <a href="#" className="nav-link m-2 menu-item nav-active text-light">About Us</a>
+            </li>
+            <li className="nav-item">
+                <a href="#" className="nav-link m-2 menu-item text-light">Projects</a>
+            </li>
+            <li className="nav-item">
+                <a href="#" className="nav-link m-2 menu-item text-light">Products</a>
+            </li>
+            <li className="nav-item">
+                <a href="#" className="nav-link m-2 menu-item text-light">Contact Us</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+  )
 }
