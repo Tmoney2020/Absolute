c26df0ef12dfd285f932530ed332d24d94b02655
Trent Costa <trentcosta@gmail.com>
2021-01-05 15:37:56 -0500

    Refactored front page and footer

---
+++ ClientApp/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+  export function Footer(props) {
+    return (
+      <footer>
+      <p>Absolute Flooring & Kitchen</p>
+      <p>4305 Clark Rd, Sarasota, FL 34233</p>
+      <p>
+        <a href="#" className="footer-contact">Contact Us</a>
+      </p>
+    </footer>
+    )
+  }
---
+++ ClientApp/src/pages/FrontPage.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+
+export function FrontPage(props) {
+
+  return (
+  <>
+    <div className="jumbotron m-5">
+      <h1 className="display-5">Absolute Flooring & Kitchen Design</h1>
+      <p className="lead">Exceptional Quality at Afforable Prices!</p>
+      <hr className="my-4" />
+      <p>Contact us for a free estimate</p>
+      <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+      <div className="job-grid">
+        <div className="job m-5">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFC9h2YBWRhibWKbdpHlLQpvDUmUcnzXCzSVaPsd_1FyS_tPebe1tNR-i8qQ&usqp=CAc" className="mr-3" alt="..." />
+          <div>
+            <h5 className="mt-1">
+              <a href="#">Job 1</a>
+            </h5>
+            Cras sit amet nibh libero, in gravida nulla.Nulla vel metus scelerisque ante sollicitudin.Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.Fusce condimentum nunc ac nisi vulputate fringilla.Donec lacinia congue felis in faucibus.
+          </div>
+        </div>
+        <div className="job m-5">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGrm4L0My-OtgctZeBxQnqe9cNJWeYbrCJnw&usqp=CAU" className="grid-picture" alt="..." />
+          <div>
+            <h5 className="mt-1">
+              <a href="#">Job 2</a>
+            </h5>
+            Cras sit amet nibh libero, in gravida nulla.Nulla vel metus scelerisque ante sollicitudi.Donec lacinia congue felis in faucibus.
+          </div>
+        </div>
+        <div className="job m-5">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFC9h2YBWRhibWKbdpHlLQpvDUmUcnzXCzSVaPsd_1FyS_tPebe1tNR-i8qQ&usqp=CAc" className="mr-3" alt="..." />
+          <div>
+            <h5 className="mt-1">
+              <a href="#">Job 3</a>
+            </h5>
+            Cras sit amet nibh libero, in gravida nulla.Nulla vel metus scelerisque ante sollicitudin.Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.Fusce condimentum nunc ac nisi vulputate fringilla.Donec lacinia congue felis in faucibus.
+          </div>
+        </div>
+        <div className="job m-5">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFC9h2YBWRhibWKbdpHlLQpvDUmUcnzXCzSVaPsd_1FyS_tPebe1tNR-i8qQ&usqp=CAc" className="mr-3" alt="..." />
+          <div>
+            <h5 className="mt-1">
+              <a href="#">Job 4</a>
+            </h5>
+            Cras sit amet nibh libero, in gravida nulla.Nulla vel metus scelerisque ante sollicitudin.Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.Fusce condimentum nunc ac nisi vulputate fringilla.Donec lacinia congue felis in faucibus.
+          </div>
+        </div>
+      </div>
+  </>
+  )
+}
