2b9fc1867a190a3253f48c344d344c55c7a2f205
Trent Costa <trentcosta@gmail.com>
2021-01-19 14:02:44 -0500

    removed service worker

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
@@ -13,7 +12,4 @@ ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
     <App />
   </BrowserRouter>,
-  rootElement);
-
-registerServiceWorker();
-
+  rootElement)
